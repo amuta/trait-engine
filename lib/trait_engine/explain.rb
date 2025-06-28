1c0b3683d5c0c16e28b0942ae40added34e61ee3
Andre Muta <andremuta@gmail.com>
2025-06-28 00:08:55 -0300

    yay

--- lib/trait_engine/explain.rb
+++
@@ -1,3 +0,0 @@
-TraitEngine::DSL.build { … } # (temporary Ruby DSL in test)
-processor.resolve_attribute(:promo_code, ctx)  # returns value
-processor.explain(:promo_code, ctx)            # JSON trace
--- lib/trait_engine/schema_compiler.rb
+++ lib/trait_engine/schema_compiler.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# # frozen_string_literal: true
 require_relative "schema"
 require_relative "decision_table"
 require_relative "trait"
